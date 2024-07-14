1c4c5e34e53de3b912fea48555cff4d814b6db5b
Adam <ADWX@novonordisk.com>
2024-07-14 03:17:28 +0200

    Fixed the post issue.

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import getCsrfToken from '../services/csrfTokenService';
 function Test() {
 
     //make a state to store the version
     let [version, setVersion] = React.useState('init');
 
-    const fetchData = () => {
+    //function for the get Button
+    const fetchDataGet = () => {
         let url = 'http://localhost:8080/version';
         fetch(url)
         .then(response => response.json())
@@ -15,28 +17,36 @@ function Test() {
     }
 
     const fetchDataPost =async () => {
-        const csrfToken = await getCsrfToken();
-        const data = {text:"!hi peachy" };
-
-
-        let url = 'http://127.0.0.1:8080/version';
-        const response = await fetch(url , {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'X-XSRF-TOKEN': csrfToken // Include the CSRF token in the header
-            },
-            credentials: 'include', // Ensure cookies are sent with the request
-            body: JSON.stringify(data)
+        getCsrfToken()
+        .then( csrfToken => {
+            console.log('fetchDataPost, i have the token.');
+            console.log(csrfToken);
+            const data = {text:"!hi peachy" };
+            
+            let url = 'http://localhost:8080/version';
+            fetch(url , {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-XSRF-TOKEN': csrfToken // Include the CSRF token in the header
+                },
+                credentials: 'include', // Ensure cookies are sent with the request
+                body: JSON.stringify(data)
+            })
+            .then(data => {
+                console.log('fetchDataPost, i am posting.');
+                return data.json();
+            })
+            .then(responseData => {
+                console.log(responseData.version);
+                setVersion(responseData.version);
+            });
         });
-        console.log('fetchDataPost, i am posting.');
-        const responseData = await response.json();
-        await setVersion(responseData.version);
     }
 
     return (
         <div>
-            <button onClick={fetchData}> Click-Get </button>
+            <button onClick={fetchDataGet}> Click-Get </button>
             <br></br>
             <button onClick={fetchDataPost}> Click-Post </button>
             <h1>{version}</h1>
@@ -46,12 +56,27 @@ function Test() {
 
 
     // Function to obtain the CSRF token from the server
-    async function getCsrfToken() {
-        const response = await fetch('http://localhost:8080/root/test', { method: 'GET', credentials: 'include', mode: 'cors'});
-        const data = await response.json();
-        await console.log(data.csrfToken);
-        return data.csrfToken; // Assuming the CSRF token is available in the response data
-    }
+    // function getCsrfToken() {
+    //     return new Promise((resolve, reject) => {
+    //         fetch('http://localhost:8080/root/test', { method: 'GET', credentials: 'include', mode: 'cors'})
+    //         .then(response => {
+    //             const headersObj = {};
+    //             response.headers.forEach((value, name) => {
+    //                 headersObj[name] = value;
+    //             });
+    //             return headersObj;
+    //         })
+    //         .then(data => {
+    //             console.log(data['x-xsrf-token']);
+    //             let token = data['x-xsrf-token'];
+    //             resolve(token);
+    //         })
+    //         .catch(error => {
+    //         // If an error occurs during the operation, reject the promise
+    //         reject(error);
+    //         });
+    //     });
+    // }
 }
 
 export default Test;
---
+++ src/services/csrfTokenService.js
@@ -0,0 +1,23 @@
+function getCsrfToken() {
+    return new Promise((resolve, reject) => {
+        fetch('http://localhost:8080/root/test', { method: 'GET', credentials: 'include', mode: 'cors'})
+        .then(response => {
+            const headersObj = {};
+            response.headers.forEach((value, name) => {
+                headersObj[name] = value;
+            });
+            return headersObj;
+        })
+        .then(data => {
+            console.log('I am here.');
+            let token = data['x-xsrf-token'];
+            resolve(token);
+        })
+        .catch(error => {
+        // If an error occurs during the operation, reject the promise
+        reject(error);
+        });
+    });
+}
+
+export default getCsrfToken;
